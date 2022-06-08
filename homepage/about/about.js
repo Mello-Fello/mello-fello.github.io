51358435969638915b0988ab291f39c2ad0fbb5d
Mello-Fello <88409500+Mello-Fello@users.noreply.github.com>
2022-06-08 02:22:30 -0800

    Delete homepage directory

--- homepage/about/about.js
+++
@@ -1,3 +0,0 @@
-function home() {
-    window.location.href = '/homepage/HTMLPage1.html'
-}
--- homepage/contact.js
+++
@@ -1,7 +0,0 @@
-//opens mail and send stuff ig
-function mail() {
-    window.location.href = "mailto:mello2fello@gmail.com?subject=Mellofly&body=Enter your message here"
-    window.location.href = "HTMLPage1.html"
-}
-
-mail();
